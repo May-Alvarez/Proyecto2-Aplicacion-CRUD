494056fdd2e13ca15bc6fb4c5545bf5ec7a2a64a
Mayra <m.alvarez0598@gmail.com>
2023-11-11 18:11:28 -0600

    Creacion del JS

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+// Seleccionar Elementos
+const form = document.getElementById("todoForm")
+
+// Evento submit del Formulario
+form.addEventListener('submit', function(event){
+    event.preventDefault();
+
+
+})
